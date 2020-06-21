bf8b0fe02b0eb74c083c714e3998b4578f1d7ef2
JatinAgrawal94 <jatinagrawal0801@gmail.com>
2020-06-21 12:56:55 +0530

    serve change

--- client/script.js
+++ client/script.js
@@ -12,7 +12,7 @@ var addTask=document.getElementById('add')
 var TASKID=0;
 data={
 	taskid:"",
-	taskname:""
+	taskname:"",
 }
 document.cookie="name="+TASKID;
 // Popup declarations
@@ -78,6 +78,8 @@ taskSubmit.addEventListener('click',()=>{
 	window.localStorage.setItem('todoarray',JSON.stringify(todoarray));
 	TASKID++;
 	document.cookie="name="+TASKID;
+	
+	
 })
 
 // addTask.addEventListener('keyup',checkKeyPress,false);	
@@ -88,72 +90,74 @@ class bar{
 
 	createBar(data){
 
-		var box=document.createElement('div');
-		box.style="float:left;background-color:yellow;width:98%;height:4em;border:2px solid black;margin-top:1em;display:flex;"
-		box.classList.add('box');
+			var box=document.createElement('div');
+			box.style="float:left;background-color:yellow;width:100%;height:4em;border:2px solid black;margin-top:1em;display:flex;"
+			box.classList.add('box');
 
-		var check=document.createElement('input');
-		check.setAttribute('type','checkbox');
-		check.style="float: left;  margin-top:1.7em;margin-left:1.5em;height:1.3rem;width:1.3rem;"
-		check.classList.add('check')
-		check.addEventListener('click',()=>{
-			if(check.checked)
+			var check=document.createElement('input');
+			check.setAttribute('type','checkbox');
+			check.style="float: left;  margin-top:1.7em;margin-left:1.5em;height:1.3rem;width:1.3rem;"
+			check.classList.add('check')
+			check.addEventListener('click',()=>{
+				if(check.checked)
+				{
+					this.remove(box,data);
+				}
+			});
+
+			var tid=document.createElement('p');
+			tid.style="float:left;margin-top:1rem;margin-left:1em;font-size:25px;"
+			tid.textContent=data.taskid
+			tid.value=data.taskid;
+
+			var tname=document.createElement('p');
+			tname.style="float:left;margin-top:1rem;margin-left:1em;font-size:25px;width:19%;text-align:center;";
+			tname.textContent=data.taskname;
+			
+
+			var tstatus=document.createElement('select')
+			tstatus.style="margin-left:3em;font-size: 16px;width:19%;text-align:center;height:2em;margin-top:1em;"
+			var options=["Not Started","In progress","In Review","Completed","Cancelled"];
+
+			for(let i=0;i<5;i++)
 			{
-				this.remove(box,data);
+				var opt=document.createElement("option");
+				opt.value=options[i];
+				opt.textContent=options[i];
+				tstatus.appendChild(opt);
 			}
-		});
 
-		var tid=document.createElement('p');
-		tid.style="float:left;margin-top:1rem;margin-left:1em;font-size:25px;"
-		tid.textContent=data.taskid
-		tid.value=data.taskid;
+			var due=document.createElement('input');
+			due.setAttribute('type','date');
+			due.style="margin-left:4em;font-size: 15px;width:20%;text-align:center;height:2em;margin-top:1em;"
+			due.value=taskDue.value;
 
-		var tname=document.createElement('p');
-		tname.style="float:left;margin-top:1rem;margin-left:1em;font-size:25px;width:19%;text-align:center;";
-		tname.textContent=data.taskname;
-		
+			var priority=document.createElement('select')
+			priority.style=" margin-left:4em;font-size: 15px;width:19%;text-align:center;height:2em;margin-top:1em;"
+			
+			
+			var prio=["Top","Middle","Last"];
 
-		var tstatus=document.createElement('select')
-		tstatus.style="margin-left:3em;font-size: 16px;width:19%;text-align:center;height:2em;margin-top:1em;"
-		var options=["Not Started","In progress","In Review","Completed","Cancelled"];
+			for(let i=0;i<3;i++)
+			{
+				var p=document.createElement("option")
+				p.textContent=prio[i];
+				priority.appendChild(p);
+			}
+			
+			box.appendChild(check);
+			box.appendChild(tid);
+			box.appendChild(tname);
+			box.appendChild(tstatus);
+			box.appendChild(due);
+			box.appendChild(priority);
+			taskArea.appendChild(box);
+			popUp.style="display:none;"
 
-		for(let i=0;i<5;i++)
+		}
+
+		remove(box,id,data)
 		{
-			var opt=document.createElement("option");
-			opt.value=options[i];
-			opt.textContent=options[i];
-			tstatus.appendChild(opt);
-		}
-
-		var due=document.createElement('input');
-		due.setAttribute('type','date');
-		due.style="margin-left:4em;font-size: 15px;width:20%;text-align:center;height:2em;margin-top:1em;"
-		due.value=taskDue.value;
-
-		var priority=document.createElement('select')
-		priority.style=" margin-left:4em;font-size: 15px;width:19%;text-align:center;height:2em;margin-top:1em;"
-		
-		
-		var prio=["Top","Middle","Last"];
-
-		for(let i=0;i<3;i++)
-		{
-			var p=document.createElement("option")
-			p.textContent=prio[i];
-			priority.appendChild(p);
-		}
-		
-		box.appendChild(check);
-		box.appendChild(tid);
-		box.appendChild(tname);
-		box.appendChild(tstatus);
-		box.appendChild(due);
-		box.appendChild(priority);
-		taskArea.appendChild(box);
-		popUp.style="display:none;"
-		}
-
-		remove(box,id,data){
 			box.parentNode.removeChild(box);
 			let index=()=>{
 				for(let i=0;i<todoarray.length;i++)
@@ -166,6 +170,7 @@ class bar{
 			todoarray.splice(index,1);
 			window.localStorage.setItem('todoarray',JSON.stringify(todoarray));
 		}
+
 }
 
 function taskValue(number){  //function to control the value of taskId
@@ -196,4 +201,3 @@ for(let v=0;v<todoarray.length;v++)
 {
 	new bar(todoarray[v]);
 }
-
--- server/server.js
+++ server/server.js
@@ -3,19 +3,19 @@ var app=express();
 var bodyparser=require('body-parser')
 var fs=require('fs');
 
-app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
 app.use(express.static('client'));
 
-app.put('/addtask',(req,res)=>{
-    // fs.appendFileSync('jatin.txt',JSON.stringify(req.body),(err,item)=>{
-    //     if(err) throw err;
-    //     console.log("writing done");
-    // })   
-    res.send(console)
+app.get('/',(req,res)=>{
+    res.send();
+})
+
+app.post('/addtask',(req,res)=>{
+    res.redirect('/');
 })
 
 app.listen(3000,()=>{
     console.log("server is running at 3000");
 })
+
